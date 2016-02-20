f091e8a4432cf350e3bb5a7f36047a86b841d3dd
Henry Till <henrytill@gmail.com>
2016-02-19 22:10:59 -0500

    project: update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,19 @@ import sbt._
 
 object Dependencies {
 
-  val akkaVersion = "2.4.1"
-
-  val akkaHttpVersion = "2.0.3"
+  val akkaVersion = "2.4.2"
 
   val coreDeps = Seq(
-    "com.h2database"         %  "h2"                          % "1.4.190",
-    "com.typesafe.akka"      %% "akka-actor"                  % akkaVersion,
-    "com.typesafe.akka"      %% "akka-http-core-experimental" % akkaHttpVersion,
-    "com.typesafe.akka"      %% "akka-http-experimental"      % akkaHttpVersion,
-    "com.typesafe.akka"      %% "akka-http-xml-experimental"  % akkaHttpVersion,
-    "com.typesafe.akka"      %% "akka-testkit"                % akkaVersion     % "test",
-    "com.typesafe.slick"     %% "slick"                       % "3.1.1",
-    "org.slf4j"              %  "slf4j-nop"                   % "1.6.4",
-    "org.scala-lang.modules" %% "scala-xml"                   % "1.0.5",
-    "org.scalatest"          %% "scalatest"                   % "2.2.6"         % "test"
+    "com.h2database"         %  "h2"                                % "1.4.190",
+    "com.typesafe.akka"      %% "akka-actor"                        % akkaVersion,
+    "com.typesafe.akka"      %% "akka-http-core"                    % akkaVersion,
+    "com.typesafe.akka"      %% "akka-http-experimental"            % akkaVersion,
+    "com.typesafe.akka"      %% "akka-http-spray-json-experimental" % akkaVersion,
+    "com.typesafe.akka"      %% "akka-http-xml-experimental"        % akkaVersion,
+    "com.typesafe.akka"      %% "akka-testkit"                      % akkaVersion % "test",
+    "com.typesafe.slick"     %% "slick"                             % "3.1.1",
+    "org.scala-lang.modules" %% "scala-xml"                         % "1.0.5",
+    "org.scalatest"          %% "scalatest"                         % "2.2.6"     % "test",
+    "org.slf4j"              %  "slf4j-nop"                         % "1.7.5"
   )
 }
