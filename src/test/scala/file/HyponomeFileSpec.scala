58bf7575fa719acf95fa4c000b565bf08a32c8eb
Henry Till <henrytill@gmail.com>
2016-02-22 20:02:11 -0500

    test: update HyponomeFileSpec

--- src/test/scala/file/HyponomeFileSpec.scala
+++ src/test/scala/file/HyponomeFileSpec.scala
@@ -3,44 +3,97 @@ package hyponome.file
 import com.typesafe.config.{Config, ConfigFactory}
 import hyponome.core._
 import java.net.InetAddress
-import java.nio.file.{FileSystem, FileSystems, Path}
+import java.nio.file._
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{Matchers, WordSpecLike}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class TestStore(p: Path) extends HyponomeFile {
 
   val storePath: Path = p
 }
 
-class HyponomeFileSpec extends WordSpecLike with Matchers {
-
-  val add = Addition(
-    SHA256Hash("01814411d889d10d474fff484e74c0f90ff5259e241de28851c2561b4ceb28a7"),
-    "ShouldMLbeOO.pdf",
-    "application/pdf",
-    164943,
-    Some(InetAddress.getByName("192.168.1.253"))
-  )
-
-  val hyponomeConfigFile: java.io.File = new java.io.File("hyponome.conf")
-
-  val config: Config = ConfigFactory.parseFile(hyponomeConfigFile)
-
-  val configStorePath: String = config.getString("file-store.path")
+class HyponomeFileSpec extends WordSpecLike with Matchers with ScalaFutures {
 
   val fs: FileSystem = FileSystems.getDefault()
 
+  /**
+    * val hyponomeConfigFile: java.io.File = new java.io.File("hyponome.conf")
+    *
+    * val config: Config = ConfigFactory.parseFile(hyponomeConfigFile)
+    *
+    * val configStorePath: Path = fs.getPath(config.getString("file-store.path"))
+    */
+
+  val tempStorePath: Path = fs.getPath("/tmp/hyponome/store")
+
+  val testPDF: Path = {
+    val s: String = getClass.getResource("/test.pdf").getPath
+    fs.getPath(s)
+  }
+
+  val testPDFHash = SHA256Hash(
+    "eba205fb9114750b2ce83db62f9c2a15dd068bcba31a2de32d8df7f7c8d85441"
+  )
+
+  /**
+    * val add = Addition(
+    *   testPDFHash,
+    *   testPDF.toFile.getName,
+    *   "application/pdf",
+    *   testPDF.toFile.length,
+    *   Some(InetAddress.getByName("192.168.1.253"))
+    * )
+    */
+
+  private def makeSimpleFileVisitor(f: Path => Unit): SimpleFileVisitor[Path] =
+    new SimpleFileVisitor[Path] {
+      override def visitFile(p: Path, attrs: attribute.BasicFileAttributes): FileVisitResult = {
+        f(p)
+        FileVisitResult.CONTINUE
+      }
+    }
+
+  private def deletePathAndParent(p: Path): Unit = {
+    Files.delete(p)
+    Files.delete(p.getParent)
+  }
+
+  def recursiveDeletePath(p: Path): Unit = {
+    Files.walkFileTree(p, makeSimpleFileVisitor(deletePathAndParent))
+    Files.delete(p)
+  }
+
   def withTestStoreInstance(testCode: TestStore => Any): Unit = {
-    val t: TestStore = new TestStore(fs.getPath(configStorePath))
+    Files.createDirectory(tempStorePath)
+    val t: TestStore = new TestStore(tempStorePath)
     testCode(t)
+    recursiveDeletePath(tempStorePath)
   }
 
   "An instance of a class that extends HyponomeFile" must {
 
+    "have a getSHA256Hash method" which {
+      "returns the correct SHA256 hash for a given file" in withTestStoreInstance { t =>
+        t.getSHA256Hash(testPDF) should equal(testPDFHash)
+      }
+    }
+
     "have a getFilePath method" which {
       "returns the correct Path" in withTestStoreInstance { t =>
-        val (dir, file) = add.hash.value.splitAt(2)
-        val expected: Path = fs.getPath(configStorePath, dir, file).toAbsolutePath
-        t.getFilePath(add.hash) should equal(expected)
+        val hash = t.getSHA256Hash(testPDF)
+        val (dir, file) = hash.value.splitAt(2)
+        val expected: Path = tempStorePath.resolve(dir).resolve(file).toAbsolutePath
+        t.getFilePath(hash) should equal(expected)
+      }
+    }
+
+    "have a copyToStore method" which {
+      "copies a file to the correct Path" in withTestStoreInstance { t =>
+        val sourceHash = t.getSHA256Hash(testPDF)
+        val destinationPath: Path = t.copyToStore(sourceHash, testPDF).futureValue
+        val destinationHash: SHA256Hash = t.getSHA256Hash(destinationPath)
+        destinationHash should equal(sourceHash)
       }
     }
   }
