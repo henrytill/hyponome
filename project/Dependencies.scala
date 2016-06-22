246eb13438843e01852add8be7e303c083d7f526
Henry Till <henrytill@gmail.com>
2016-06-21 22:47:20 -0400

    project/Dependencies.scala: update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,15 @@ import sbt._
 
 object Dependencies {
 
-  val http4sVersion = "0.14.0a-SNAPSHOT"
+  lazy val argonautVersion = "6.1a"
 
-  val scalazVersion = "7.2.2"
+  lazy val http4sVersion = "0.14.1a"
 
-  val commonDeps = Seq(
+  lazy val scalazVersion = "7.2.4"
+
+  lazy val scalazStreamVersion = "0.8.2a"
+
+  lazy val commonDeps = Seq(
     "ch.qos.logback"      % "logback-classic"     % "1.1.3",
     "commons-codec"       % "commons-codec"       % "1.10",
     "org.scalacheck"     %% "scalacheck"          % "1.12.5" % "test",
@@ -15,18 +19,16 @@ object Dependencies {
     "org.scalaz"         %% "scalaz-concurrent"   % scalazVersion,
     "org.slf4j"           % "slf4j-api"           % "1.7.5")
 
-  val coreDeps = Seq(
+  lazy val coreDeps = Seq(
     "com.h2database"      % "h2"                  % "1.4.190",
     "com.typesafe.slick" %% "slick"               % "3.1.1")
 
-  val httpDeps = Seq(
+  lazy val httpDeps = Seq(
     "com.typesafe"        % "config"              % "1.3.0",
-    "io.argonaut"        %% "argonaut"            % "6.2-SNAPSHOT" changing(),
-    "io.argonaut"        %% "argonaut-scalaz"     % "6.2-SNAPSHOT" changing(),
-    "io.argonaut"        %% "argonaut-monocle"    % "6.2-SNAPSHOT" changing(),
+    "io.argonaut"        %% "argonaut"            % argonautVersion,
     "org.http4s"         %% "http4s-argonaut"     % http4sVersion % "test",
     "org.http4s"         %% "http4s-dsl"          % http4sVersion,
     "org.http4s"         %% "http4s-blaze-server" % http4sVersion,
     "org.http4s"         %% "http4s-blaze-client" % http4sVersion,
-    "org.scalaz.stream"  %% "scalaz-stream"       % "0.8a")
+    "org.scalaz.stream"  %% "scalaz-stream"       % scalazStreamVersion)
 }
