a7a26c8634d6df66407c8d07fd81ec4d3a6ed25e
Henry Till <henrytill@gmail.com>
2017-05-28 22:24:16 -0400

    core: add sizedByteArray generator

--- core/src/test/scala/hyponome/test/Generators.scala
+++ core/src/test/scala/hyponome/test/Generators.scala
@@ -23,4 +23,6 @@ trait Generators {
   val genNEByteArray: Gen[Array[Byte]] = Gen.nonEmptyContainerOf[Array, Byte](Arbitrary.arbitrary[Byte])
 
   val genNEListOfNEByteArrays: Gen[List[Array[Byte]]] = Gen.nonEmptyContainerOf[List, Array[Byte]](genNEByteArray)
+
+  def sizedByteArray(size: Int): Gen[Array[Byte]] = Gen.listOfN(size, Arbitrary.arbitrary[Byte]).map(_.toArray)
 }
