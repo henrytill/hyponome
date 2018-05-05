1a6ebd87c38e65e04535456fa70c48b5ea4cb9c5
Henry Till <henrytill@gmail.com>
2018-05-05 18:09:04 -0400

    common.nix: add epstopdf

--- common.nix
+++ common.nix
@@ -2,11 +2,15 @@
 
 rec {
 
+  texliveEnv = with pkgs; texlive.combine { inherit (texlive) scheme-basic epstopdf; };
+
   buildDeps = with pkgs;
     [ catch
       cmake
       doxygen
+      ghostscript
       pkgconfig
+      texliveEnv
     ];
 
   deps = with pkgs;
